fb6a5dfd09db3de2f5c3d5fe6f409bdab46182ad
Jorge Luis Avendaño Cervantes <42230297+jork1990@users.noreply.github.com>
2024-03-13 21:00:36 -0400

    Update CConexion.java

--- src/main/java/Clases/CConexion.java
+++ src/main/java/Clases/CConexion.java
@@ -6,6 +6,7 @@ package Clases;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+
 import javax.swing.JOptionPane;
 
 /**
@@ -13,53 +14,48 @@ import javax.swing.JOptionPane;
  * @author jorge
  */
 public class CConexion {
-    
-Connection conectar = null;
-    
-    String usuario="root";
-    String contrasenia="Isaac2014";
-    String bd="bdusuarios";
-    String ip="localhost";
-    String puerto="3306";
-    
-    String cadena ="jdbc:mysql://"+ip+":"+puerto+"/"+bd;
-    
-    public Connection estableceConexion(){
-    
+
+    Connection conectar = null;
+
+    String usuario = "root";
+    String contrasenia = "";
+    String bd = "bdusuarios";
+    String ip = "localhost";
+    String puerto = "3306";
+
+    String cadena = "jdbc:mysql://" + ip + ":" + puerto + "/" + bd;
+
+    public Connection estableceConexion() {
+
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            conectar = DriverManager.getConnection(cadena,usuario,contrasenia);
-            
-           // JOptionPane.showMessageDialog(null, "Se conectó a la BD correctamente");
-            
+            conectar = DriverManager.getConnection(cadena, usuario, contrasenia);
+
+            // JOptionPane.showMessageDialog(null, "Se conectó a la BD correctamente");
+
         } catch (Exception e) {
-            
+
             JOptionPane.showMessageDialog(null, "NO se conectó a la BD correctamente");
             JOptionPane.showMessageDialog(null, "Conexion Realizada");
         }
-    
-        return  conectar;
-    
+
+        return conectar;
+
     }
-    
-    public void cerrarConexion(){
-   
+
+    public void cerrarConexion() {
+
         try {
-            if (conectar!= null && !conectar.isClosed()) {
-                
+            if (conectar != null && !conectar.isClosed()) {
+
                 conectar.close();
-               // JOptionPane.showMessageDialog(null, "Conexion Cerrada");
+                // JOptionPane.showMessageDialog(null, "Conexion Cerrada");
             }
         } catch (Exception e) {
-            
+
             JOptionPane.showMessageDialog(null, "No se pudo cerrar la conexion");
         }
-   
-        
-        
+
     }
 
-    
-    
 }
-
